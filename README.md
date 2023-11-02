193e501916091426d778b8268fbaac910a7d2839
jaxe93 <jaxe93work@gmail.com>
2023-11-02 18:24:33 -0400

    added inquirer

---
+++ README.md
@@ -0,0 +1,30 @@
+## USER build story
+
+For this project I wanted to create a logo maker that would create logos as SVG files for future projects, and assignments.  I made It so that it would be capable of picking between a circle, square, or triangle for the background image.  It would allow me to input the text I wanted to put for it, and to be able to change the color for both the image, and the text.  I was able to achieve this using node, and Java.  I also was able to use the input suite for inquier, and the test suite jest to test this out. After creation of the new SVG files I also got them to be saved to a folder called examples to be able to catelog any new emblems created.  This is going to be a very handy application to be able to use for future projects, and I would like to make changes for it in the future.  I followed these acceptance criteria.  
+
+```md
+GIVEN a command-line application that accepts user input
+WHEN I am prompted for text
+THEN I can enter up to three characters
+WHEN I am prompted for the text color
+THEN I can enter a color keyword (OR a hexadecimal number)
+WHEN I am prompted for a shape
+THEN I am presented with a list of shapes to choose from: circle, triangle, and square
+WHEN I am prompted for the shape's color
+THEN I can enter a color keyword (OR a hexadecimal number)
+WHEN I have entered input for all the prompts
+THEN an SVG file is created named `logo.svg`
+AND the output text "Generated logo.svg" is printed in the command line
+WHEN I open the `logo.svg` file in a browser
+THEN I am shown a 300x200 pixel image that matches the criteria I entered
+```
+
+I have completed these to the best of my ability, and have showcased this off on the git hub, and made a video showing the functionality of the assignment. located at the links below.
+
+https://github.com/jaxe93/SVG-Logo-Maker
+
+https://drive.google.com/file/d/1RLIK5UwMfT8l-Z4mPuJo0iYjRFMZA_a2/viewLinks to an external site.\
+
+## In the future
+
+For the future of the application, I would like to be able to add more shapes to be used to label creation.  I think that this would be able to keep using it well into the future  for many future projects and assignments!  
--- index.js
+++ index.js
@@ -1,62 +1,6 @@
-/*import fs from "fs"
-import { Triangle, Circle, Square } from "./lib/shapes.js"
-import rl from "readline" */
 const inquirer = require('inquirer');
 var fs = require("fs")
 var { Triangle, Circle, Square } = require("./lib/shapes.js")
-
-const readline = require("readline").createInterface({
-    input:process.stdin,
-    output:process.stdout
-});
-
-var run = () => {
-  var userText = "text"
-  var userTextColor = "#ff0000"
-  var userShape = "triangle"
-  var userShapeColor = "#ff0000"
-
-
-    var getText = () => readline.question("whats the text?", (text) => {
-        userText = text;
-        getUserTextColor()
-    })
-
-    var getUserTextColor = () => readline.question("what color would you like this text?", (textColor) => {
-        userTextColor = textColor;
-        getShape()
-    })
-
-    var getShape = () => readline.question("what shape?", (shape) => {
-        userShape = shape;
-        getShapeColor()
-    })
-
-    var getShapeColor = () => readline.question("what color would you like the shape?", (shapeColor) => {
-        userShapeColor = shapeColor;
-        readline.close()
-        if (userShape === "triangle") {
-            var shape = new Triangle(userText, userTextColor, userShape, )
-            shape.setColor(userColor)
-        }
-        if (userShape === "circle") {
-            var shape = new Circle(userText, userTextColor, userShape, userShapeColor)
-        }
-        if (userShape === "square") {
-            var shape = new Square(userText, userTextColor, userShape, userShapeColor)
-        } 
-    
-        
-        const data = new Uint8Array(Buffer.from(shape.render()));
-        fs.writeFile(`lib/examples/${userText + userTextColor + userShape + userShapeColor}.svg`, data, (err) => {
-        if (err) throw err;
-        console.log('The file has been saved!');
-        });
-    })
-
-    getText()
-
-}
 
 // save each user choice to an object
 var choices = {
@@ -65,38 +9,53 @@ var choices = {
   text: undefined,
   shape: undefined,
 }
+// inquirer check box and inputs for label selection
+const questions = [
+  {
+    type: 'checkbox',
+    name: 'select shape',
+    message: "What shape?",
+    choices: ['square', 'triangle', 'circle']
+  },
+  {
+    type: 'input',
+    name: 'shape_color',
+    message: "What color would you like the shape?",
+  },
+  {
+    type: 'input',
+    name: 'label_name',
+    message: "What's your lable name?",
+  },
+  {
+    type: 'input',
+    name: 'label_color',
+    message: "What's your lable text color?",
+  },
+];
+// calling inquirer prompts
+inquirer.prompt(questions).then((answers) => {
+  choices.shape = answers['select shape'][0];
+  choices.fillColor = answers['shape_color'];
+  choices.text = answers['label_name'];
+  choices.textColor = answers['label_color'];
 
-inquirer
-  .prompt([
-    {
-      type: 'checkbox',
-      message: 'would you like to make an emblem?',
-      
-      name: 'your choice',
-      choices: [
-        new inquirer.Separator(' = The shapes = '),
-        {
-          name: 'circle',
-        },
-        {
-          name: 'square',
-        },
-        {
-          name: 'triangle',
-        },
-        
-      ],
-      validate(answer) {
-        if (answer.length < 1) {
-          return 'You must choose one shape.';
-        }
+  // imports classes from shapes.js  creates new instance of class.
+  if (choices.shape === "triangle") {
+    var shape = new Triangle(choices.text, choices.textColor, choices.shape, )
+    shape.setColor(choices.fillColor)
+  }
+  if (choices.shape === "circle") {
+    var shape = new Circle(choices.text, choices.textColor, choices.shape, choices.fillColor)
+  }
+  if (choices.shape === "square") {
+    var shape = new Square(choices.text, choices.textColor, choices.shape, choices.fillColor)
+  } 
+// saves svg file after creation to file called examples
+  const data = new Uint8Array(Buffer.from(shape.render()));
+    fs.writeFile(`lib/examples/${choices.text + choices.textColor + choices.shape + choices.fillColor}.svg`, data, (err) => {
+      if (err) throw err;
+      console.log('The file has been saved!');
+    });
 
-        return true;
-      },
-    },
-  ])
-  .then((answers) => {
-    console.log(JSON.stringify(answers, null, '  '));
-    run ();
-  }); 
-  
+});
